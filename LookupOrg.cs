b8d2a0521e56afe2b39c091b4d41380f249271c2
Rob Relyea <rob@relyeas.net>
2024-10-25 11:45:45 -0700

    more tries

--- LookupOrg.cs
+++ LookupOrg.cs
@@ -45,46 +45,46 @@ namespace api.multifol.io
 
             try
             {
-                using (var conn = new MySqlConnection(builder.ConnectionString))
+                await using var connection = new MySqlConnection(builder.ConnectionString);
+                _logger.LogInformation("Opening connection");
+                await connection.OpenAsync();
+
+                string sql =
+                            "select *, 1 as rowOrder from organizations where " +
+                            "MATCH (Organization) AGAINST (@term IN BOOLEAN MODE); ";
+                //"UNION " +
+                //"select *, 2 as rowOrder from organizations where " +
+                //"MATCH(Organization) AGAINST(concat(@term, '* -', @term) IN BOOLEAN MODE) " +
+                //"ORDER by rowOrder, Organization";
+
+                using (var command = new MySqlCommand())
                 {
-                    Console.WriteLine("Opening connection");
-                    await conn.OpenAsync();
+                    command.Connection = connection;
+                    command.CommandText = sql;
+                    command.Parameters.AddWithValue("term", term);
 
-                    string sql =
-                                "select *, 1 as rowOrder from organizations where " +
-                                "MATCH (Organization) AGAINST (?term IN BOOLEAN MODE); ";
-                                //"UNION " +
-                                //"select *, 2 as rowOrder from organizations where " +
-                                //"MATCH(Organization) AGAINST(concat(@term, '* -', @term) IN BOOLEAN MODE) " +
-                                //"ORDER by rowOrder, Organization";
-
-                    using (var command = new MySqlCommand(sql, conn))
+                    _logger.LogInformation("Execute command");
+                    await command.ExecuteNonQueryAsync();
+                    
+                    _logger.LogInformation("Read results");
+                    using var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
                     {
-                        command.Parameters.AddWithValue("term", term);
-
-                        Console.WriteLine("Execute command");
-
-                        using (var reader = await command.ExecuteReaderAsync())
-                        {
-                            while (await reader.ReadAsync())
-                            {
-                                var EIN = reader.GetString(0);
-                                var organization = reader.GetString(1);
-                                var city = reader.GetString(2);
-                                var state = reader.GetString(3);
-                                var businessCode = reader.GetString(4);
-                                _logger.LogInformation($"{EIN}, {organization}, {city}, {state}, {businessCode}");
-                            }
-                        }
+                        var EIN = reader.GetString(0);
+                        var organization = reader.GetString(1);
+                        var city = reader.GetString(2);
+                        var state = reader.GetString(3);
+                        var businessCode = reader.GetString(4);
+                        _logger.LogInformation($"{EIN}, {organization}, {city}, {state}, {businessCode}");
                     }
-
-                    return new OkObjectResult("Returned Ok");
                 }
+
+                return new OkObjectResult("Returned Ok");
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex.Message);
-                _logger.LogError(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
                 return new BadRequestObjectResult("Returned BadRequest");
 
             }
